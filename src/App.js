561622a781f3c41a7e2fcfdf2b38b71314872175
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-20 09:46:03 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      this is app
+      <Button type="primary">Default Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+const Login=()=>{
+    return<div>
+        this is login
+    </div>
+}
+
+export default Login
