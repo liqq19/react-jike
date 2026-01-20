350c7a2d2166fadce6bdc6666c24ad50156085d5
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-20 22:25:53 +0800

    token注入

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,15 +1,16 @@
 import { request } from "@/utils"
 import { createSlice } from "@reduxjs/toolkit"
+import { setToken as _setToken ,getToken} from "@/utils/token"
 
 const userStore = createSlice({
     name:"user",
     initialState:{
-        token:localStorage.getItem('token_key') || ''
+        token:getToken() || ''
     },
     reducers:{
      setToken:(state,action)=>{
         state.token = action.payload
-        localStorage.setItem('token_key', action.payload)
+        _setToken(action.payload)
      }   
     }
 })
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,17 @@
 import axios from "axios";
+import { getToken } from "./token";
 
 const request = axios.create({
     baseURL: "http://geek.itheima.net/v1_0",
     timeout: 5000,
 });
 request.interceptors.request.use((config)=> {
-    return config
+  const token=getToken()  
+  if(token){
+      config.headers.Authorization=`Bearer ${token}`
+  }
+
+  return config
   }, (error)=> {
     return Promise.reject(error)
 })
