2ae926bc67e9ede0361e01678dbad47c23657b8e
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-20 13:07:52 +0800

    登录-维护token数据

---
+++ src/store/modules/user.js
@@ -0,0 +1,26 @@
+import { request } from "@/utils"
+import { createSlice } from "@reduxjs/toolkit"
+
+const userStore = createSlice({
+    name:"user",
+    initialState:{
+        token:""
+    },
+    reducers:{
+     setToken:(state,action)=>{
+        state.token = action.payload
+     }   
+    }
+})
+const{setToken} = userStore.actions
+const userReducer = userStore.reducer
+const fetchLogin=(loginForm)=>{
+    return async(dispatch)=>{
+        const res = await request.post('/authorizations',loginForm)
+        dispatch(setToken(res.data.token))
+    }
+}
+
+export { setToken, fetchLogin }
+export default userReducer
+
