743f3ea9e177347fae6c55845740bcf9241032b4
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-21 09:06:15 +0800

    2级路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,28 @@ import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRouts } from "@/components/AuthRouts";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element:<AuthRouts><Layout/></AuthRouts>
+        element:<AuthRouts><Layout/></AuthRouts>,
+        children:[
+            {
+                path:"home",
+                element:<Home/>
+            },
+            {
+                path:"article",
+                element:<Article/>
+            },
+            {
+                path:"publish",
+                element:<Publish/>
+            }
+        ]
     },
     {
         path:"/login",
