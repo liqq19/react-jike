abe2dbd916980c5feceb72b0c008c6f592746398
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-21 14:54:25 +0800

    表格封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,12 @@
+import BarChart from './componemts/BarChart'
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+    <div>
+      <BarChart title={'三大框架满意度'}/>
+      <BarChart title={'三大框架使用度'}/>
+    </div>
+  )
 };
 
 export default Home;
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -15,7 +15,7 @@ const { Header, Sider } = Layout
 const items = [
   {
     label: '首页',
-    key: '/',
+    key: '/home',
     icon: <HomeOutlined />,
   },
   {
