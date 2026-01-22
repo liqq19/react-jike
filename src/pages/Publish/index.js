84051769cacdb3de8835c4120224230b2ce54945
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-22 22:47:40 +0800

    图片上传类型

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -50,6 +50,11 @@ const Publish = () => {
     console.log('上传中',value)
     setImageList(value.fileList)
   }
+  const[imageType,setImageType]=useState(0)
+  const onTypeChange=(e)=>{
+    console.log('切换封面了',e.target.value)
+    setImageType(e.target.value)
+  }
   return (
     <div className="publish">
       <Card
@@ -64,7 +69,7 @@ const Publish = () => {
         <Form
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ type: 1 }}
+          initialValues={{ type: 0 }}
           onFinish={onFinish}
         >
           <Form.Item
@@ -85,13 +90,13 @@ const Publish = () => {
           </Form.Item>
           <Form.Item label="封面">
           <Form.Item name="type">
-            <Radio.Group>
+            <Radio.Group onChange={onTypeChange}>
               <Radio value={1}>单图</Radio>
               <Radio value={3}>三图</Radio>
               <Radio value={0}>无图</Radio>
             </Radio.Group>
           </Form.Item>
-          <Upload
+          {imageType>0&&<Upload
             listType="picture-card"
             showUploadList
             action={'http://geek.itheima.net/v1_0/upload'}
@@ -101,7 +106,8 @@ const Publish = () => {
             <div style={{ marginTop: 8 }}>
               <PlusOutlined />
             </div>
-          </Upload>
+          </Upload>}
+          
           </Form.Item>
           <Form.Item
             label="内容"
