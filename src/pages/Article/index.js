0c4354c126e1bae74e3ba341ecd95be026583323
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-25 21:30:34 +0800

    文章列表-状态

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -13,6 +13,10 @@ const { RangePicker } = DatePicker
 
 const Article = () => {
   const{channelList}=useChannel()
+  const status={
+    1:<Tag color='warning'>待审核</Tag>,
+    2:<Tag color='success'>审核通过</Tag>
+  }
   const columns = [
     {
       title: '封面',
@@ -30,7 +34,7 @@ const Article = () => {
     {
       title: '状态',
       dataIndex: 'status',
-      render: data => <Tag color="green">审核通过</Tag>
+      render: data => status[data]
     },
     {
       title: '发布时间',
