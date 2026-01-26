e70fdc853e51451ab3948b27e28c2095bece4faa
李zhenhua <16588158+li-zh-en-hua@user.noreply.gitee.com>
2026-01-26 20:50:10 +0800

    文章列表-编辑id发布和编辑

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -66,7 +66,9 @@ const Publish = () => {
           return {url}
         }))
     }
-    getArticleDetail()
+    if (articleId) {
+      getArticleDetail()
+    }
   },[articleId,form])
   return (
     <div className="publish">
@@ -74,7 +76,7 @@ const Publish = () => {
         title={
           <Breadcrumb items={[
             { title: <Link to={'/'}>首页</Link> },
-            { title: '发布文章' },
+            { title: `${articleId?'编辑':'发布'}文章` },
           ]}
           />
         }
